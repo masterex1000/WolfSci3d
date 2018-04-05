7605cc13fab15b41f2a7ffb2725c3e70299a4006
masterex1000 <masterex1000@users.noreply.github.com>
2018-04-05 10:57:48 -0600

    Add files via upload

---
+++ WolfSie/desktop/src/com/wolfsie/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wolfsie.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wolfsie.game.WolfSieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WolfSieGame(), config);
+	}
+}
---
+++ WolfSie/html/src/com/wolfsie/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.wolfsie.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.wolfsie.game.WolfSieGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WolfSieGame();
+        }
+}
